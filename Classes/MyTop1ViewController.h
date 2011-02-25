4a624d2e8a942ce821e3c300252acd4e36977096
elomarns <elomarns@gmail.com>
2011-02-25 10:19:57 -0300

    Improving code formating of main view controller header.

--- Classes/MyTop1ViewController.h
+++ Classes/MyTop1ViewController.h
@@ -1,16 +1,7 @@
-//
-//  MyTop1ViewController.h
-//  MyTop1
-//
-//  Created by Elomar Nascimento dos Santos on 2/23/11.
-//  Copyright 2011 not a serious company. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
-@interface MyTop1ViewController : UIViewController {
-
+@interface MyTop1ViewController : UIViewController 
+{
 }
 
 @end
-
