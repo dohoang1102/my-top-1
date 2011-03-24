d0a55efbfb7229bb39aef95477c47928a898b440
elomarns <elomarns@gmail.com>
2011-03-23 22:49:01 -0300

    Improving code format of main.m.

--- main.m
+++ main.m
@@ -1,17 +1,12 @@
-//
-//  main.m
-//  MyTop1
-//
-//  Created by Elomar Nascimento dos Santos on 2/23/11.
-//  Copyright 2011 not a serious company. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
-int main(int argc, char *argv[]) {
-    
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    int retVal = UIApplicationMain(argc, argv, nil, nil);
-    [pool release];
-    return retVal;
+int main(int argc, char *argv[]) 
+{    
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+  
+  int retVal = UIApplicationMain(argc, argv, nil, nil);
+  
+  [pool release];
+  
+  return retVal;
 }
