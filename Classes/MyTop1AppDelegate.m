5e727dc591c11299396e483bba86d477d17fc754
elomarns <elomarns@gmail.com>
2011-03-25 00:17:51 -0300

    Improving MyTop1AppDelegate.m.

--- Classes/MyTop1AppDelegate.m
+++ Classes/MyTop1AppDelegate.m
@@ -32,16 +32,19 @@
   return [[NSUserDefaults standardUserDefaults] stringForKey: @"FavoriteNumber"];
 }
 
+- (void)showMyTop1View
+{
+  [self.window addSubview: self.viewController.view];
+  [self.window makeKeyAndVisible];
+}
+
 #pragma mark -
 #pragma mark Application lifecycle
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions 
-{
+{  
   if(![self favoriteNumber])
-  {
-    [self.window addSubview: viewController.view];
-    [self.window makeKeyAndVisible];
-  }  
+    [self showMyTop1View];
 
   return YES;
 }
@@ -49,7 +52,17 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application 
 {
   if([self mustResetFavoriteNumber])
+  {
+    if([[self.window subviews] count] == 0)
+      [self showMyTop1View];
+      
+    viewController.instructionsLabel.text = 
+      @"You haven't set your favorite person yet. Tap the button below to set it, and reset it on My Top 1 preferences on Settings app.";
+    
+    [viewController.setFavoritePersonButton setTitle: @"Set your favorite person" forState: UIControlStateNormal];
+    
     [self resetFavoriteNumber];
+  }
   
   if([self favoriteNumber])
     [Call toNumber: [self favoriteNumber]];
